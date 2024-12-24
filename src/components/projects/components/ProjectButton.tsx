230cb2e3a18afafaea77ec01dd8d114734033571
Dominic Petriella <dompetriella@gmail.com>
2024-12-24 15:10:06 -0500

    Adjusted color

--- src/components/projects/components/ProjectButton.tsx
+++ src/components/projects/components/ProjectButton.tsx
@@ -1,3 +1,4 @@
+import App from "../../../App";
 import { useAppState } from "../../../state/AppState";
 import AppColors from "../../../theme/AppColors";
 
@@ -12,20 +13,36 @@ export function ProjectButton({ buttonText }: { buttonText: string }) {
     ? AppColors.white
     : AppColors.black;
 
+  const mainButtonShadowColor = lightModeState ? AppColors.darkBlueAuxilary : AppColors.darkGreyTertiary
+
+  const splashBackgroundColor = lightModeState
+    ? AppColors.lightBlueSecondary
+    : AppColors.lightGreyTertiary;
+
+
+
   return (
     <>
       <div
         style={{ zIndex: 10 }}
         className="relative flex justify-center items-center w-full p-8 h-60"
       >
-        <div className="absolute w-52 h-52 bottom-3 bg-red-600 rounded-full"></div>
-        <div className="absolute w-44 h-44 bottom-5 bg-black rounded-full"></div>
+        <div style={{ backgroundColor: splashBackgroundColor, opacity: lightModeState ? 0.5 : 0.1 }} className="absolute w-52 h-52 bottom-3 rounded-full"></div>
+        <div style={{ backgroundColor: splashBackgroundColor, opacity: lightModeState ? 0.8 : 0.5 }} className="absolute w-44 h-44 bottom-5 rounded-full"></div>
+        <div
+          style={{
+            backgroundColor: mainButtonShadowColor,
+            opacity: 0.50
+          }}
+          className="absolute w-36 h-36 bottom-[24px] rounded-full "
+        >
+        </div>
         <button
           style={{
             backgroundColor: mainButtonBackgroundColor,
             color: mainButtonTextColor,
           }}
-          className="absolute w-36 h-36 text-2xl bottom-7  rounded-full flex justify-center items-center tracking-widest"
+          className="absolute w-36 h-36 text-3xl bottom-7  rounded-full flex justify-center items-center tracking-widest"
         >
           {buttonText}
         </button>
